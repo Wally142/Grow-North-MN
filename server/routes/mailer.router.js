33233ecc254268d906ce00f46b8b1863c132f532
Evan Kearney <evanjkearney@gmail.com>
2017-11-03 15:57:51 -0500

    Reset email generates route with random token

--- server/routes/mailer.router.js
+++ server/routes/mailer.router.js
@@ -1,32 +1,56 @@
 var router = require('express').Router();
 var path = require('path');
+var async = require('async');
+var crypto = require('crypto');
 
 var nodemailer = require('nodemailer');
 
 router.get('/', function(req, res){
-    var transporter = nodemailer.createTransport({
-        service: 'Hotmail',
-        auth: {
-            user: 'evanmobile@hotmail.com',
-            pass: 'im@sk00l'
-        }
-    });
+    async.waterfall([
+        function(done) {
+          crypto.randomBytes(20, function(err, buffer) {
+            var token = buffer.toString('hex');
+            done(err, token);
+          });
+        },
+        // function(token, done) {
+        //   User.findOne({ email: req.body.email }, function(err, user) {
+        //     if (!user) {
+        //       req.flash('error', 'No account with that email address exists.');
+        //       return res.redirect('/forgot');
+        //     }
     
-    var mailOptions = {
-        from: 'evanmobile@hotmail.com',
-        to: 'evanjkearney@gmail.com',
-        subject: 'test email',
-        text: 'this is the test email sent with nodemailer'
-    };
+        //     user.resetPasswordToken = token;
+        //     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
     
-    transporter.sendMail(mailOptions, function(err, info){
-        if (err){
-            console.log(err);
-        }else{
-            console.log('Email sent:', info.response);
+        //     user.save(function(err) {
+        //       done(err, token, user);
+        //     });
+        //   });
+        // },
+        function(token, done) {
+            var transporter = nodemailer.createTransport({
+                service: 'Hotmail',
+                auth: {
+                    user: 'evanmobile@hotmail.com',
+                    pass: 'im@sk00l'
+                }
+            });
+            var mailOptions = {
+                from: 'evanmobile@hotmail.com',
+                to: 'evanjkearney@gmail.com',
+                subject: 'Grow North App Password Reset',
+                html: '<p>You\'re receiving this email because a password reset request was sent to the Grow North App.</p>' +
+                '<a href="http://localhost:5000/#/mailer/' + token + '">Click here to reset password</a>' +
+                '<p>If you didn\'t make this request... that\'s pretty concerning.</p>' 
+            };
+            transporter.sendMail(mailOptions, function(err, info){
+                done(err, 'done');
+            });
         }
-    });
-    res.sendStatus(200);
+      ], function(err) {
+        if (err) return next(err);
+      });
 })
 
 module.exports = router;

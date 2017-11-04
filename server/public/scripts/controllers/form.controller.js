c6150d017674ce81593b07b9e9a6febf8e6ecd8c
Mike Pettman <mp_palmatum@yahoo.com>
2017-11-04 13:19:19 -0500

    broken radio buttons

--- server/public/scripts/controllers/form.controller.js
+++ server/public/scripts/controllers/form.controller.js
@@ -1,19 +1,55 @@
-myApp.controller('FormController', function(FormService) {
+myApp.controller('FormController', function (FormService) {
   console.log('FormController created');
   var vm = this;
 
   vm.page = 0;
 
+  vm.radioSelect = null;
+
   vm.formInput = {
-    name: '',
-    email: '',
-    phone: '',
-    company: '',
-    referral: ''
+    name: null,
+    email: null,
+    phone: null,
+    company: null,
+    title: null,
+    referral: null,
+    permission: false,
+    mentor: false,
+    mentee: false,
+    involvement: null,
+    howHelp: null,
+    experience: null,
+    struggle: null,
+    ecosystem: null,
+    employees: null,
+    revenue: null,
+    distribution: null,
+    story: null,
+    legal: false
   };
 
-  vm.grabInputs = function(){
+  vm.detectRadio = function () {
+    switch (vm.radioSelect) {
+      case 'mentor':
+        vm.formInput.mentor = true;
+        vm.formInput.mentee = false;
+        break;
+      case 'mentee':
+        vm.formInput.mentor = false;
+        vm.formInput.mentee = true;
+        break;
+      case 'both':
+        vm.formInput.mentor = true;
+        vm.formInput.mentee = true;
+        break;
+    }
+  }
+
+  vm.grabInputs = function () {
     console.log('vm.formInput', vm.formInput);
+    FormService.addContact(vm.formInput);
   };
+
+
 
 });

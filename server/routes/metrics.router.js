8b8a533c89457f7fbaa24ca424ded263860a04cd
Evan Kearney <evanjkearney@gmail.com>
2017-11-10 15:20:08 -0600

    Add authentication check to appropriate server routes

--- server/routes/metrics.router.js
+++ server/routes/metrics.router.js
@@ -3,66 +3,78 @@ var router = express.Router();
 var pool = require('../modules/pool.js');
 
 router.get('/mentors', function (req, res) {
-    console.log('get Mentors');
-    pool.connect(function (error, client, done) {
-        if (error) {
-            console.log(error);
-            res.sendStatus(404);
-        } else {
-            client.query("SELECT * FROM prospects WHERE mentor = true;", function (queryErr, resultObj) {
-                done();
-                if (queryErr) {
-                    console.log(queryErr)
-                    res.sendStatus(500);
-                } else {
-                    console.log(resultObj.rows);
-                    res.send(resultObj.rows);
-                }
-            });
-        }
-    });
+    if (req.isAuthenticated()){
+        console.log('get Mentors');
+        pool.connect(function (error, client, done) {
+            if (error) {
+                console.log(error);
+                res.sendStatus(404);
+            } else {
+                client.query("SELECT * FROM prospects WHERE mentor = true;", function (queryErr, resultObj) {
+                    done();
+                    if (queryErr) {
+                        console.log(queryErr)
+                        res.sendStatus(500);
+                    } else {
+                        console.log(resultObj.rows);
+                        res.send(resultObj.rows);
+                    }
+                });
+            }
+        });
+    }else{
+        res.sendStatus(403);
+    }
 });// end  GET mentors
 
 router.get('/mentees', function (req, res) {
-    console.log('get Mentees');
-    pool.connect(function (error, client, done) {
-        if (error) {
-            console.log(error);
-            res.sendStatus(404);
-        } else {
-            client.query("SELECT * FROM prospects WHERE mentee = true;", function (queryErr, resultObj) {
-                done();
-                if (queryErr) {
-                    console.log(queryErr)
-                    res.sendStatus(500);
-                } else {
-                    console.log("mentEE", resultObj.rows);
-                    res.send(resultObj.rows);
-                }
-            });
-        }
-    });
+    if (req.isAuthenticated()){
+        console.log('get Mentees');
+        pool.connect(function (error, client, done) {
+            if (error) {
+                console.log(error);
+                res.sendStatus(404);
+            } else {
+                client.query("SELECT * FROM prospects WHERE mentee = true;", function (queryErr, resultObj) {
+                    done();
+                    if (queryErr) {
+                        console.log(queryErr)
+                        res.sendStatus(500);
+                    } else {
+                        console.log("mentEE", resultObj.rows);
+                        res.send(resultObj.rows);
+                    }
+                });
+            }
+        });
+    }else{
+        res.sendStatus(403);
+    }
 });// end Get mentees
 
 router.get('/connections', function (req, res) {
-    console.log('get connections');
-    pool.connect(function (error, client, done) {
-        if (error) {
-            console.log(error);
-            res.sendStatus(404);
-        } else {
-            client.query("SELECT * FROM connections;", function (queryErr, resultObj) {
-                done();
-                if (queryErr) {
-                    console.log(queryErr)
-                    res.sendStatus(500);
-                } else {
-                    console.log("connections", resultObj.rows);
-                    res.send(resultObj.rows);
-                }
-            });
-        }
-    });
+    if (req.isAuthenticated()){
+        console.log('get connections');
+        pool.connect(function (error, client, done) {
+            if (error) {
+                console.log(error);
+                res.sendStatus(404);
+            } else {
+                client.query("SELECT * FROM connections;", function (queryErr, resultObj) {
+                    done();
+                    if (queryErr) {
+                        console.log(queryErr)
+                        res.sendStatus(500);
+                    } else {
+                        console.log("connections", resultObj.rows);
+                        res.send(resultObj.rows);
+                    }
+                });
+            }
+        });
+    }else{
+        res.sendStatus(403);
+    }
 });// end Get connections
 
 

ca6af08b96a96bfa1d29bf790f1edf82c04edcea
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-11-14 14:47:43 -0600

    paginate directory

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages', 'mdCollectionPagination']);
 
 /// Routes ///
 myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {

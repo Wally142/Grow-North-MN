1aefd75e933f38ad1e52612ec3592af8273e70b7
Mike Pettman <mp_palmatum@yahoo.com>
2017-11-16 16:28:15 -0600

    edit changes

--- server/database.sql
+++ server/database.sql
@@ -1,38 +1,68 @@
 CREATE DATABASE grow-north;
 
-CREATE TABLE prospects (
- id serial primary key,
- firstname VARCHAR (200),
- lastname VARCHAR (200),
- email VARCHAR (200),
- phone VARCHAR (20),
- company VARCHAR (200),
- image VARCHAR (3000),
- title VARCHAR (200),
- referral VARCHAR (600),
- permission BOOLEAN default false,
- mentor BOOLEAN default false,
- mentee BOOLEAN default false,
- involvement VARCHAR (200),
- howhelp VARCHAR (600),
- experience VARCHAR (1000),
- struggle VARCHAR (2000),
- ecosystem VARCHAR (200),
- employees VARCHAR (600),
- revenue VARCHAR (600),
- distribution VARCHAR (200),
- story VARCHAR (1000),
- comments VARCHAR (3000),
- startdate TIMESTAMP DEFAULT current_timestamp,
- approved BOOLEAN default false,
- tags TEXT[]
- );
+ CREATE TABLE prospects (
+  id serial primary key,
+  firstname VARCHAR (200),
+  lastname VARCHAR (200),
+  email VARCHAR (200),
+  phone VARCHAR (20),
+  linkedin VARCHAR (200),
+  website VARCHAR (200),
+  company VARCHAR (200),
+  title VARCHAR (200),
+  referral VARCHAR (600),
+  permission BOOLEAN default false,
+  mentor BOOLEAN default false,
+  mentee BOOLEAN default false,
+  involvement TEXT[],
+  howhelp TEXT[],
+  experience VARCHAR (1000),
+  ecosystem TEXT[],
+  employees VARCHAR (10),
+  revenue VARCHAR (20),
+  distribution VARCHAR (200),
+  story VARCHAR (1000),
+  comments VARCHAR (3000),
+  startdate TIMESTAMP DEFAULT current_timestamp,
+  approved BOOLEAN default false,
+  tags TEXT[]
+  );
+
+-- CREATE TABLE prospects (
+--   id serial primary key,
+--   firstname VARCHAR (200),
+--   lastname VARCHAR (200),
+--   email VARCHAR (200),
+--   phone VARCHAR (20),
+--   linkedin VARCHAR (200),
+--   website VARCHAR (200),
+--   company VARCHAR (200),
+--   title VARCHAR (200),
+--   referral VARCHAR (600),
+--   permission BOOLEAN default false,
+--   mentor BOOLEAN default false,
+--   mentee BOOLEAN default false,
+--   involvement VARCHAR (200),
+--   howhelp VARCHAR (600),
+--   experience VARCHAR (1000),
+--   ecosystem VARCHAR (200),
+--   employees VARCHAR (600),
+--   revenue VARCHAR (600),
+--   distribution VARCHAR (200),
+--   story VARCHAR (1000),
+--   comments VARCHAR (3000),
+--   startdate TIMESTAMP DEFAULT current_timestamp,
+--   approved BOOLEAN default false,
+--   tags TEXT[]
+-- );
 
 CREATE TABLE connections (
 id serial primary key,
 person1 int references prospects (id) ON DELETE CASCADE,
 person2 int references prospects (id) ON DELETE CASCADE,
-constraint person1_cannot_be_equal_to_person2_CHK CHECK (person1 <> person2)
+constraint person1_cannot_be_equal_to_person2_CHK CHECK (person1 <> person2),
+date TIMESTAMP DEFAULT current_timestamp,
+comments VARCHAR(3000)
 );
 
 CREATE TABLE users (

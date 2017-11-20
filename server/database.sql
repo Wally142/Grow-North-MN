285fedc23d769115e52d6e81b01cdf9a5bb7d94d
Cam <cam.schnackel@gmail.com>
2017-11-20 14:09:01 -0600

    fixed connection card styling issue

--- server/database.sql
+++ server/database.sql
@@ -28,34 +28,6 @@ CREATE DATABASE grow-north;
   anythingelse VARCHAR (1000)
   );
 
--- CREATE TABLE prospects (
---   id serial primary key,
---   firstname VARCHAR (200),
---   lastname VARCHAR (200),
---   email VARCHAR (200),
---   phone VARCHAR (20),
---   linkedin VARCHAR (200),
---   website VARCHAR (200),
---   company VARCHAR (200),
---   title VARCHAR (200),
---   referral VARCHAR (600),
---   permission BOOLEAN default false,
---   mentor BOOLEAN default false,
---   mentee BOOLEAN default false,
---   involvement VARCHAR (200),
---   howhelp VARCHAR (600),
---   experience VARCHAR (1000),
---   ecosystem VARCHAR (200),
---   employees VARCHAR (600),
---   revenue VARCHAR (600),
---   distribution VARCHAR (200),
---   story VARCHAR (1000),
---   comments VARCHAR (3000),
---   startdate TIMESTAMP DEFAULT current_timestamp,
---   approved BOOLEAN default false,
---   tags TEXT[]
--- );
-
 CREATE TABLE connections (
 id serial primary key,
 person1 int references prospects (id) ON DELETE CASCADE,

fc55088df84d3134aa567a419fd85cd52260c5ff
Evan Kearney <evanjkearney@gmail.com>
2017-11-26 16:30:49 -0600

    Implement pagination

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages', 'mdCollectionPagination', 'angAccordion']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages', 'mdCollectionPagination', 'angularUtils.directives.dirPagination']);
 
 /// Routes ///
 myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
